1d63f31a901b39e27ddef1f52a1814eab37d6f3e
Firmino Changani <firmino.changani@gmail.com>
2021-03-06 13:59:51 +0000

    feat: Generate post slug in build time

--- build.prerender.js
+++ build.prerender.js
@@ -1,18 +1,43 @@
+require("dotenv/config");
 const path = require("path");
+const axios = require("axios");
+const log4js = require("log4js");
+const logger = log4js.getLogger();
+logger.level = "debug";
+
+axios.default.defaults.baseURL = process.env.VUE_APP_API;
+
+/**
+ * getPostsSlugs: Fetch the API to retrieve post slugs
+ * during build time
+ */
+async function getPostsSlugs() {
+  try {
+    const { data: posts } = await axios.get("/posts");
+    return posts.map(post => {
+      logger.info(`Generating slug: ${post.slug}__${post.id}...`);
+      return `${post.slug}__${post.id}`;
+    });
+  } catch (error) {
+    logger.error("An error occurred while fetching posts");
+    logger.error(error);
+    logger.error("Exiting...");
+    process.exit(1);
+  }
+}
 
 module.exports = (api, options) => {
-  api.registerCommand("build:prerender", async (args) => {
+  api.registerCommand("build:prerender", async args => {
     const PrerenderSPAPlugin = require("prerender-spa-plugin");
-    // const { data } = await axios.get("http://some-api.com/companies");
-    const posts = ["1", "2", "3"];
-    api.chainWebpack((config) => {
+    const posts = await getPostsSlugs();
+    api.chainWebpack(config => {
       config.plugin("prerender").use(PrerenderSPAPlugin, [
         {
           // Required - The path to the webpack-outputted app to prerender.
           staticDir: path.join(__dirname, "dist"),
           // Required - Routes to render.
-          routes: ["/"].concat(posts.map((post) => `/posts/${post}`)),
-        },
+          routes: ["/"].concat(posts.map(post => `/blog/${post}`))
+        }
       ]);
     });
 
@@ -21,5 +46,5 @@ module.exports = (api, options) => {
 };
 
 module.exports.defaultModes = {
-  "build:prerender": "production",
+  "build:prerender": "production"
 };

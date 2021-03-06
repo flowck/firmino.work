930fd6e75494cb0b4de46d3a4ec782eb14d7eaf4
Firmino Changani <firmino.changani@gmail.com>
2021-03-06 16:49:28 +0000

    chore: Remove page config

--- src/components/_app-comments.vue
+++ src/components/_app-comments.vue
@@ -1,26 +1,15 @@
 <template>
   <div class="app-comments">
-    <Disqus :shortname="DISQUS_SHORTNAME" :pageConfig="pageConfig"></Disqus>
+    <Disqus :shortname="DISQUS_SHORTNAME"></Disqus>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component } from "vue-property-decorator";
 const { VUE_APP_DISQUS_SHORTNAME } = process.env;
 
 @Component
 export default class Comments extends Vue {
   private readonly DISQUS_SHORTNAME = VUE_APP_DISQUS_SHORTNAME;
-
-  @Prop({ required: true })
-  private title!: string;
-
-  @Prop({ required: true })
-  private slug!: string;
-
-  get pageConfig() {
-    const { title, slug } = this;
-    return { title, slug };
-  }
 }
 </script>

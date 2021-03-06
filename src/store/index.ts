a3d10d3104742b779411109c96499c05f9b2b33f
Firmino Changani <firmino.changani@gmail.com>
2021-03-06 16:50:49 +0000

    feat: Add store elements to controll isLoading state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import Vuex from "vuex";
-import { Modules } from "./contants";
 import { IRootState } from "./IRootState";
 import { PostModule } from "./modules/postModule";
+import { Actions, Modules, Mutations } from "./contants";
+import Vuex, { GetterTree, ActionTree, MutationTree } from "vuex";
 
 Vue.use(Vuex);
 
@@ -10,9 +10,26 @@ const rootState: IRootState = {
   isLoading: false
 };
 
+const getters: GetterTree<IRootState, IRootState> = {
+  isLoading: (state: IRootState) => state.isLoading
+};
+
+const actions: ActionTree<IRootState, IRootState> = {
+  [Actions.SetLoading]: ({ commit }, status: boolean) => {
+    commit(Mutations.SET_LOADING, status);
+  }
+};
+
+const mutations: MutationTree<IRootState> = {
+  [Mutations.SET_LOADING]: (state: IRootState, status: boolean) => {
+    state.isLoading = status;
+  }
+};
+
 export default new Vuex.Store({
-  actions: {},
-  mutations: {},
+  getters,
+  actions,
+  mutations,
   state: rootState,
   modules: { [Modules.POST_MODULE]: PostModule }
 });

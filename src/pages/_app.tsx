db6430288c5593886272f249f1c55ebca3f5a62e
Firmino Changani <firmino.changani@gmail.com>
2022-11-20 15:12:39 +0000

    update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 // import "../styles/globals.css";
-import "../../public/assets/css/prism.css";
+import { Open_Sans } from "@next/font/google";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ReactNode } from "react";
-import { Open_Sans } from "@next/font/google";
 import { globalStyles } from "stitches.config";
+import "../../public/assets/css/prism.css";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 const Noop = ({ children }: { children: ReactNode }) => children;
@@ -16,6 +16,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="icon" href="/assets/img/favicon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png" />
+        <link rel="manifest" href="/assets/site.webmanifest" />
       </Head>
       <style jsx global>{`
         html,
